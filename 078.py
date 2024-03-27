742464e0e8ed113d0b4488de95cdb2abad55587b
TempZzh <>
2024-03-28 00:38:06 +0800

    078.py 回溯

---
+++ 078.py
@@ -0,0 +1,12 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        comb = []
+        self.recursion(comb, [], nums, 0)
+        return comb
+
+    def recursion(self, comb, tmpList, nums, start):
+        comb.append(tmpList.copy())
+        for i in range(start, len(nums)):
+            tmpList.append(nums[i])
+            self.recursion(comb, tmpList, nums, i+1)
+            tmpList.pop()
