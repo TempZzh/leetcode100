eeaaec96464af87440ec41152af8c0f30bf37fdf
TempZzh <>
2024-03-31 01:27:37 +0800

    017.py 回溯

---
+++ 017.py
@@ -0,0 +1,28 @@
+from typing import List
+
+
+class Solution:
+    dict = {
+            '2': 'abc', '3': 'def', '4': 'ghi',
+            '5': 'jkl', '6': 'mno', '7': 'pqrs',
+            '8': 'tuv', '9': 'wxyz'
+    }
+    
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+        
+        combinations = []
+        self.dfs(digits, 0, '', combinations)
+        return combinations
+
+    def dfs(self, digits, index, path, combinations):
+        if index == len(digits):
+            combinations.append(path)
+            return
+        
+        for x in self.dict[digits[index]]:
+            self.dfs(digits, index + 1, path + x, combinations)
+
+if __name__ == '__main__':
+    print(Solution().letterCombinations('2345'))
